ed15509d777268bf5da6329ff305c934c5d677fa
MingLIUUU <liu.mingzheng@outlook.com>
2024-04-19 13:49:47 -0400

    home update

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     alert('Welcome to My Art Portfolio');
 });
 
+function toggleMenu() {
+    const navbar = document.getElementById('navbar');
+    if (navbar.style.display === "block") {
+        navbar.style.display = "none";
+    } else {
+        navbar.style.display = "block";
+    }
+}
