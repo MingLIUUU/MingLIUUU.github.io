223e8cd2846cfd51fbf3e9a34d6624778d93b6af
MingLIUUU <liu.mingzheng@outlook.com>
2024-04-20 18:21:01 -0400

    menu done

--- script.js
+++ script.js
@@ -1,38 +1,27 @@
-function toggleMenu() {
-    const navbar = document.getElementById('navbar');
-    if (navbar.style.display === "block") {
-        navbar.style.display = "none";
-    } else {
-        navbar.style.display = "block";
-        
-    }
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('nav-toggle');
+});
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('nav-toggle');
 }
 
-$(document).ready(function(){
-    $('.your-carousel-class').slick({
-        // options here
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear',
-        slidesToShow: 1,
-        adaptiveHeight: true
-    });
-});
+let slideIndex = 0;
+showSlides();
 
-document.addEventListener('DOMContentLoaded', function() {
-    const galleryImages = document.querySelectorAll('.gallery img');
-    galleryImages.forEach(img => {
-        img.addEventListener('click', function() {
-            const enlargement = document.createElement('div');
-            enlargement.innerHTML = `<div style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0,0,0,0.8); display: flex; justify-content: center; align-items: center;">
-                                        <img src="${this.src}" style="max-width: 90%; max-height: 90%;" />
-                                     </div>`;
-            enlargement.addEventListener('click', function() {
-                this.remove();
-            });
-            document.body.appendChild(enlargement);
-        });
-    });
-});
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
